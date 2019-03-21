1802fa33e7e0f8739e14c97e69297c191e0990a7
Wenyao <38875149+shawenyao@users.noreply.github.com>
2019-03-20 23:36:11 -0400

    efficient frontier functions update

--- efficient_frontier.R
+++ efficient_frontier.R
@@ -1,13 +1,15 @@
-library(tidyverse)
+suppressWarnings(library(tidyverse))
 
 setwd("C:/Users/Wenyao/Desktop/R/ETF-vs-rho")
 source("functions/functions_efficient_frontier.R", echo=TRUE)
 
+
+#==== general setup ====
 rf <- 0.01
-miu <- c(0.05, 0.15, 0.25)
-sigma <- c(0.25, 0.35, 0.50)
-rho12 <- 0.5
-rho23 <- 0.7
+miu <- c(0.05, 0.15, 0.20)
+sigma <- c(0.25, 0.3, 0.35)
+rho12 <- 0.1
+rho23 <- 0.5
 rho13 <- 0.2
 rho <- matrix(
   c(1, rho12, rho13, rho12, 1, rho23, rho13, rho23, 1), 
@@ -15,9 +17,35 @@ rho <- matrix(
   ncol = length(miu), 
   byrow = TRUE
 )
+atomic_assets <- tibble(volatility = sigma, return = miu)
 
-weight_tangent <- calcualte_tangent_portfolio(rf, miu, sigma, rho)
-print(weight_tangent)
-calculate_return(weight_tangent, miu)
-calculate_volatility(weight_tangent, sigma, rho)
 
+#==== global minimum variance portfolio ====
+weight_minimum <- calculate_minimum_variance_portfolio(miu, sigma, rho)
+minimum_portfolio <- calculate_return_volatility(list(weight_minimum), miu, sigma, rho)
+
+
+#==== tangency portfolio ====
+weight_tangency <- calculate_tangency_portfolio(rf, miu, sigma, rho)
+tangency_portfolio <- calculate_return_volatility(list(weight_tangency), miu, sigma, rho)
+
+
+#==== efficient frontier ====
+efficient_frontier_weights <- calculate_efficient_frontier(miu, sigma, rho, min_return = 0.01, max_return = 0.22, step = 0.005)
+efficient_frontier <- calculate_return_volatility(efficient_frontier_weights, miu, sigma, rho)
+
+
+#==== plot ====
+ggplot(efficient_frontier, aes(x = volatility, y = return)) +
+  # the efficient frontier
+  geom_path(size = 2, linejoin = "round", lineend = "round", color = "gray66") +
+  # the atomic assets
+  geom_point(data = atomic_assets, color = "dodgerblue3", size = 5) +
+  geom_point(data = atomic_assets, color = "white", size = 2) +
+  # the global minimum variance portfolio
+  geom_point(data = minimum_portfolio, color = "chartreuse4", size = 5) +
+  geom_point(data = minimum_portfolio, color = "white", size = 2) +
+  # the tangency portfolio
+  geom_point(data = tangency_portfolio, color = "coral3", size = 5) +
+  geom_point(data = tangency_portfolio, color = "white", size = 2) +
+  theme_minimal()
--- functions/functions_efficient_frontier.R
+++ functions/functions_efficient_frontier.R
@@ -1,50 +1,3 @@
-#' find the efficient frontier
-#' by scanning over portfolios over different weight combinations
-#' 
-#' @param miu a vector of expected return
-#' @param sigma a vector of volatility
-#' @param rho a square matrix of correlation
-#' 
-#' @return a data.frame of coordinates along the risk frontier
-#' 
-calcualte_efficient_frontier <- function(moi, sigma, rho){
-  
-  step <- 0.05
-  # scan over the entire spectrum of single-asset weight
-  weight_vector <- seq(-5, 5, step)
-  # all combination of asset weights
-  weights_vector <- expand.grid(weight_vector, weight_vector, weight_vector)
-  
-  
-  
-  
-}
-
-
-#' find the weights of the tangent portfolio
-#' 
-#' @param rf risk-free rate
-#' @param miu a vector of expected return
-#' @param sigma a vector of volatility
-#' @param rho a square matrix of correlation
-#' 
-#' @return a vector of weights
-#' 
-calcualte_tangent_portfolio <- function(rf, miu, sigma, rho){
-  
-  # the variance-covariance matrix
-  omega <- diag(sigma) %*% rho %*% diag(sigma)
-  # ensure the weights sum up to 1
-  normalizing_factor <- matrix(1, nrow = 1, ncol = ncol(omega)) %*% 
-    solve(omega) %*% 
-    (miu - rf)
-  # the weights of the tangent portflio
-  weights <- solve(omega) %*% (miu - rf) / as.numeric(normalizing_factor)
-  
-  return(weights)
-}
-
-
 #' find the expected return of the portfolio
 #' 
 #' @param weight a vector of asset weights
@@ -59,7 +12,7 @@ calculate_return <- function(weight, miu){
 }
 
 
-#' find the weights of the tangent portfolio
+#' find the weights of the tangency portfolio
 #' 
 #' @param weight a vector of asset weights
 #' @param sigma a vector of volatility
@@ -70,8 +23,125 @@ calculate_return <- function(weight, miu){
 calculate_volatility <- function(weight, sigma, rho){
   
   # the variance-covariance matrix
-  omega <- diag(sigma) %*% rho %*% diag(sigma)
+  big_sigma <- diag(sigma) %*% rho %*% diag(sigma)
   
-  # sigma = sqrt(t(weight) %*% omega %*% weight)
-  sqrt(matrix(weight, nrow = 1) %*% omega %*% matrix(weight, ncol = 1))
+  # sigma = sqrt(t(weight) %*% big_sigma %*% weight)
+  as.numeric(sqrt(matrix(weight, nrow = 1) %*% big_sigma %*% matrix(weight, ncol = 1)))
+}
+
+
+#' find the return and volatility given a list of asset weights
+#' 
+#' @param weights a list of vector of weights
+#' @param miu a vector of expected return
+#' @param sigma a vector of volatility
+#' @param rho a square matrix of correlation
+#' 
+#' @return a data.frame of expected return and volatility
+#' 
+calculate_return_volatility <- function(weights, miu, sigma, rho){
+  
+  # find the return and volatility for each set of portfolio weights
+  tibble(
+    return = weights %>% 
+      map(calculate_return, miu = miu) %>% 
+      unlist(),
+    volatility = weights %>% 
+      map(calculate_volatility, sigma = sigma, rho = rho) %>% 
+      unlist()
+  )
+}
+
+
+#' find the global minimum variance portfolio
+#' 
+#' @param miu a vector of expected return
+#' @param sigma a vector of volatility
+#' @param rho a square matrix of correlation
+#' 
+#' @return a list of the vector of weights of the global minimum variance portfolio
+#' 
+calculate_minimum_variance_portfolio <- function(miu, sigma, rho){
+  
+  # the variance-covariance matrix
+  big_sigma <- diag(sigma) %*% rho %*% diag(sigma)
+
+  # solving for a %*% x = b
+  a <- (big_sigma * 2) %>%
+    cbind(matrix(1, nrow = 3, ncol = 1)) %>% 
+    rbind(matrix(c(rep(1, times = length(miu)), 0), nrow = 1))
+  b <- matrix(c(rep(0, times = length(miu)), 1), ncol = 1)
+  
+  # return the first N elements of the solution (the weights of the portfolio)
+  solve(a, b)[seq_along(miu),1]
+}
+
+
+#' find the tangency portfolio
+#' 
+#' @param rf risk-free rate
+#' @param miu a vector of expected return
+#' @param sigma a vector of volatility
+#' @param rho a square matrix of correlation
+#' 
+#' @return a list of the vector of weights of the tangency portfolio
+#' 
+calculate_tangency_portfolio <- function(rf, miu, sigma, rho){
+  
+  # the variance-covariance matrix
+  big_sigma <- diag(sigma) %*% rho %*% diag(sigma)
+  
+  # ensure the weights sum up to 1
+  normalizing_factor <- matrix(1, nrow = 1, ncol = ncol(big_sigma)) %*% 
+    solve(big_sigma) %*% 
+    (miu - rf)
+  
+  # the weights of the tangency portflio
+  solve(big_sigma) %*% (miu - rf) / as.numeric(normalizing_factor)
+}
+
+
+#' find the portfolio with the lowest volatility given a target expected return
+#' 
+#' @param miu a vector of expected return
+#' @param sigma a vector of volatility
+#' @param rho a square matrix of correlation
+#' @param target_return the desired level of return
+#' 
+#' @return a vector of the asset weights in the desired portfolio
+#' 
+calculate_efficient_portfolio <- function(miu, sigma, rho, target_return){
+  
+  # the variance-covariance matrix
+  big_sigma <- diag(sigma) %*% rho %*% diag(sigma)
+  
+  # solving for a %*% x = b
+  a <- big_sigma %>% 
+    cbind(matrix(miu, ncol = 1), 1) %>% 
+    rbind(matrix(c(miu, 0, 0), nrow = 1)) %>% 
+    rbind(matrix(c(rep(1, times = length(miu)), 0, 0), nrow = 1))
+  b <- matrix(c(rep(0, times = length(miu)), target_return, 1), ncol = 1)
+  
+  # return the first N elements of the solution (the weights of the portfolio)
+  solve(a, b)[seq_along(miu),1]
+}
+
+
+#' find the efficient frontier
+#' 
+#' @param miu a vector of expected return
+#' @param sigma a vector of volatility
+#' @param rho a square matrix of correlation
+#' @param min_return the minimum return to be considered
+#' @param max_return the maximum return to be considered
+#' @param step the size of each step
+#' 
+#' @return a list of weights along the efficient frontier
+#' 
+calculate_efficient_frontier <- function(miu, sigma, rho, min_return, max_return, step){
+  
+  # scan over the target return range
+  # and find the portfolio with minimal volatility given a target expected return 
+  seq(min_return, max_return, by = step) %>% 
+    map(calculate_efficient_portfolio, miu = miu, sigma = sigma, rho = rho)
 }
