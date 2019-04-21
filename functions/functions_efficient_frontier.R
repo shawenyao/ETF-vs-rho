e23797ff934fa27f0a6f52b0ee69dcf2d4847f2a
Wenyao <38875149+shawenyao@users.noreply.github.com>
2019-04-20 21:13:25 -0400

    functionalize efficient frontier plotting

--- functions/functions_efficient_frontier.R
+++ functions/functions_efficient_frontier.R
@@ -145,3 +145,95 @@ calculate_efficient_frontier <- function(miu, sigma, rho, min_return, max_return
   seq(min_return, max_return, by = step) %>% 
     map(calculate_efficient_portfolio, miu = miu, sigma = sigma, rho = rho)
 }
+
+
+#' calculate and plot the efficeint frontier
+#'
+#' @param risky_assets a data.frame of the return and volatility of all risky assets
+#' @param rho the correlation matrix of all risky assets
+#' @param risk_free_asset a data.frame of the return and volatility (0) of risk-free asset
+#'
+#' @return a ggplot2 object
+#'
+plot_efficient_frontier <- function(risky_assets, rho, risk_free_asset){
+  
+  # global minimum variance portfolio
+  weight_minimum <- calculate_minimum_variance_portfolio(
+    miu = risky_assets$return,
+    sigma = risky_assets$volatility,
+    rho = rho
+  )
+  minimum_portfolio <- calculate_return_volatility(
+    weights = list(weight_minimum), 
+    miu = risky_assets$return,
+    sigma = risky_assets$volatility,
+    rho = rho
+  )
+  
+  # tangency portfolio
+  weight_tangency <- calculate_tangency_portfolio(
+    rf = risk_free_asset$return,
+    miu = risky_assets$return, 
+    sigma = risky_assets$volatility, 
+    rho = rho
+  )
+  tangency_portfolio <- calculate_return_volatility(
+    weights = list(weight_tangency),
+    miu = risky_assets$return,
+    sigma = risky_assets$volatility,
+    rho = rho
+  )
+  
+  # efficient frontier
+  weights_efficient_frontier <- calculate_efficient_frontier(
+    miu = risky_assets$return,
+    sigma = risky_assets$volatility,
+    rho = rho,
+    min_return = 0.001, 
+    max_return = 0.25, 
+    step = 0.005
+  )
+  efficient_frontier <- calculate_return_volatility(
+    weights = weights_efficient_frontier,
+    miu = risky_assets$return, 
+    sigma = risky_assets$volatility, 
+    rho = rho
+  )
+  
+  # tangency line
+  tangency_line <- tibble(
+    volatility = seq(from = 0, to = 0.365, by = 0.01)
+  ) %>% 
+    mutate(
+      return = (tangency_portfolio$return - risk_free_asset$return) /
+        tangency_portfolio$volatility * volatility + 
+        risk_free_asset$return
+    )
+  
+  # return the plot
+  ggplot(efficient_frontier, aes(x = volatility, y = return)) +
+    # the efficient frontier
+    geom_path(size = 2, linejoin = "round", lineend = "round", color = "gray66") +
+    # the tangency line
+    geom_path(data = tangency_line, linetype = "dashed", size = 1.5, lineend = "round", color = "gray85") +
+    # the atomic assets
+    geom_point(data = risky_assets, color = "dodgerblue3", size = 5) +
+    geom_point(data = risky_assets, color = "white", size = 2) +
+    # the global minimum variance portfolio
+    geom_point(data = minimum_portfolio, color = "chartreuse4", size = 5) +
+    geom_point(data = minimum_portfolio, color = "white", size = 2) +
+    # the tangency portfolio
+    geom_point(data = tangency_portfolio, color = "coral3", size = 5) +
+    geom_point(data = tangency_portfolio, color = "white", size = 2) +
+    # the risk-free rate
+    geom_point(data = risk_free_asset, color = "chocolate1", size = 5) +
+    geom_point(data = risk_free_asset, color = "white", size = 2) +
+    xlim(0, max(efficient_frontier$volatility)) +
+    theme_minimal() +
+    theme(
+      text = element_text(size = 15),
+      axis.title.x = element_text(margin = margin(t = 15, r = 0, b = 0, l = 0)),
+      axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0))
+    ) +
+    labs(x = "Volatility", y = "Return")
+}
--- post_efficient_frontier.R
+++ post_efficient_frontier.R
@@ -9,73 +9,30 @@ source("functions/functions_save_svg.R", echo = TRUE)
 
 
 #==== general setup ====
-rf <- 0.01
-miu <- c(0.05, 0.15, 0.16)
-sigma <- c(0.20, 0.24, 0.28)
+risky_assets <- tibble(return = c(0.05, 0.15, 0.16), volatility = c(0.20, 0.24, 0.28))
+risk_free_asset <- tibble(return = 0.01, volatility = 0)
+
+# correlation matrix
 rho12 <- 0.1
 rho23 <- 0.5
 rho13 <- 0.2
 rho <- matrix(
   c(1, rho12, rho13, rho12, 1, rho23, rho13, rho23, 1), 
-  nrow = length(miu), 
-  ncol = length(miu), 
+  nrow = nrow(risky_assets), 
+  ncol = nrow(risky_assets), 
   byrow = TRUE
 )
-atomic_assets <- tibble(volatility = sigma, return = miu)
-risk_free <- tibble(volatility = 0, return = rf)
 
 
-#==== global minimum variance portfolio ====
-weight_minimum <- calculate_minimum_variance_portfolio(miu, sigma, rho)
-minimum_portfolio <- calculate_return_volatility(list(weight_minimum), miu, sigma, rho)
+#==== plot =====
+plot1 <- plot_efficient_frontier(
+  risky_assets = risky_assets, 
+  rho = rho,
+  risk_free_asset = risk_free_asset
+)
 
 
-#==== tangency portfolio ====
-weight_tangency <- calculate_tangency_portfolio(rf, miu, sigma, rho)
-tangency_portfolio <- calculate_return_volatility(list(weight_tangency), miu, sigma, rho)
-
-
-#==== efficient frontier ====
-weights_efficient_frontier <- calculate_efficient_frontier(miu, sigma, rho, min_return = 0.001, max_return = 0.25, step = 0.005)
-efficient_frontier <- calculate_return_volatility(weights_efficient_frontier, miu, sigma, rho)
-
-
-#==== tangency line ====
-tangency_line <- tibble(
-  volatility = seq(from = 0, to = 0.365, by = 0.01)
-) %>% 
-  mutate(
-    return = (tangency_portfolio$return - rf) / tangency_portfolio$volatility * volatility + rf
-  )
-
-
-#==== plot ====
-plot1 <- ggplot(efficient_frontier, aes(x = volatility, y = return)) +
-  # the efficient frontier
-  geom_path(size = 2, linejoin = "round", lineend = "round", color = "gray66") +
-  # the tangency line
-  geom_path(data = tangency_line, linetype = "dashed", size = 1.5, lineend = "round", color = "gray85") +
-  # the atomic assets
-  geom_point(data = atomic_assets, color = "dodgerblue3", size = 5) +
-  geom_point(data = atomic_assets, color = "white", size = 2) +
-  # the global minimum variance portfolio
-  geom_point(data = minimum_portfolio, color = "chartreuse4", size = 5) +
-  geom_point(data = minimum_portfolio, color = "white", size = 2) +
-  # the tangency portfolio
-  geom_point(data = tangency_portfolio, color = "coral3", size = 5) +
-  geom_point(data = tangency_portfolio, color = "white", size = 2) +
-  # the risk-free rate
-  geom_point(data = risk_free, color = "chocolate1", size = 5) +
-  geom_point(data = risk_free, color = "white", size = 2) +
-  xlim(0, max(efficient_frontier$volatility)) +
-  theme_minimal() +
-  theme(
-    text = element_text(size = 15),
-    axis.title.x = element_text(margin = margin(t = 15, r = 0, b = 0, l = 0)),
-    axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0))
-  ) +
-  labs(x = "Volatility", y = "Return")
-
+#==== output ====
 save_svg(plot = plot1, file_name = "output/efficient_frontier1.svg", width = 5, height = 3)
 
 # play sound when finished
