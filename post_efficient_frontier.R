78a67b70db7706411ca7b2bb73b69ba9dfe2655c
Wenyao <38875149+shawenyao@users.noreply.github.com>
2019-04-21 14:53:44 -0400

    add final state plot

--- post_efficient_frontier.R
+++ post_efficient_frontier.R
@@ -10,13 +10,14 @@ source("functions/functions_save_svg.R", echo = TRUE)
 
 #==== general setup ====
 risky_assets <- tibble(return = c(0.05, 0.15, 0.16), volatility = c(0.20, 0.24, 0.28))
+risky_assets_equilibrium <- tibble(return = c(0.10, 0.118, 0.136), volatility = c(0.20, 0.24, 0.28))
 risk_free_asset <- tibble(return = 0.01, volatility = 0)
 
 # correlation matrix
 rho12 <- 0.1
 rho23 <- 0.5
 rho13 <- 0.2
-rho_equ <- 0.9999999
+rho_perfect <- 0.9999999
 rho <- matrix(
   c(1, rho12, rho13, rho12, 1, rho23, rho13, rho23, 1), 
   nrow = nrow(risky_assets), 
@@ -24,7 +25,7 @@ rho <- matrix(
   byrow = TRUE
 )
 rho_equilibrium <- matrix(
-  c(1, rho_equ, rho_equ, rho_equ, 1, rho_equ, rho_equ, rho_equ, 1), 
+  c(1, rho_perfect, rho_perfect, rho_perfect, 1, rho_perfect, rho_perfect, rho_perfect, 1), 
   nrow = nrow(risky_assets), 
   ncol = nrow(risky_assets), 
   byrow = TRUE
@@ -47,11 +48,21 @@ plot2 <- plot_efficient_frontier(
   y_range = c(0, 0.25)
 )
 
+plot3 <- plot_efficient_frontier(
+  risky_assets = risky_assets_equilibrium, 
+  rho = rho_equilibrium,
+  risk_free_asset = risk_free_asset,
+  x_range = c(0, 0.44),
+  y_range = c(0, 0.25)
+)
+
 
 #==== output ====
 save_svg(plot = plot1, file_name = "output/efficient_frontier1.svg", width = 5, height = 3)
 
 save_svg(plot = plot2, file_name = "output/efficient_frontier2.svg", width = 5, height = 3)
+
+save_svg(plot = plot3, file_name = "output/efficient_frontier3.svg", width = 5, height = 3)
 
 # play sound when finished
 beep(sound = 2)
